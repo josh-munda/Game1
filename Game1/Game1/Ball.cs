65451670f970dae62e19fa89e550a16a0ffec4ca
josh-munda <Munda@ksu.edu>
2023-09-14 10:38:20 -0500

    Paddle class done. Finish ball class with adding the center and radius and collision checks with paddles

---
+++ Game1/Game1/Ball.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    public class Ball
+    {
+        private Texture2D texture;
+
+        private Vector2 position;
+
+        private Vector2 velocity;
+
+        /// <summary>
+        /// The constructor for the ball's position and velocity
+        /// </summary>
+        public Ball()
+        {
+            position = new Vector2(180, 150);
+            velocity = new Vector2(5, 5);
+        }
+
+        /// <summary>
+        /// Loads the sprite texture using the provided ContentManager
+        /// </summary>
+        /// <param name="content">The ContentManager to load with</param>
+        public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("ball");
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+            //Update the position based on the velocity
+            position += velocity;
+
+            //Check for collision with the top and bottom of the screen
+            if(position.Y < 0 || position.Y + texture.Height > 360)
+            {
+                velocity.Y *= -1;
+            }
+        }
+
+        /// <summary>
+        /// Draw the ball on the screen
+        /// </summary>
+        /// <param name="spriteBatch">The spriteBatch to draw the sprite</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}
---
+++ Game1/Game1/Paddle.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    public class Paddle
+    {
+        private GamePadState gamePadState;
+
+        private KeyboardState keyboardState;
+
+        private Texture2D texture;
+
+        private Vector2 position;
+
+        private int initialXPositionForPlayer1 = 0;
+        private int initialYPositionForPlayer1 = 180;
+
+        private int initialXPositionForPlayer2 = 715;
+        private int initialYPositionForPlayer2 = 180;
+
+        public static readonly int screenHeight = 360;
+
+        /// <summary>
+        /// The color to blend with the paddle
+        /// </summary>
+        public Color Color { get; set; } = Color.White;
+
+        /// <summary>
+        /// The constructor for the paddles and its positions
+        /// </summary>
+        /// <param name="isPlayer1">If it is Player1</param>
+        public Paddle(bool isPlayer1)
+        {
+            if (isPlayer1)
+            {
+                position = new Vector2(initialXPositionForPlayer1, initialYPositionForPlayer1);
+            }
+            else
+            {
+                position = new Vector2(initialXPositionForPlayer2, initialYPositionForPlayer2);
+            }
+        }
+
+        /// <summary>
+        /// Loads the sprite texture using the provided ContentManager
+        /// </summary>
+        /// <param name="content">The ContentManager to load with</param>
+        public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("paddle");
+        }
+
+        /// <summary>
+        /// Updates the sprite's position based on user input
+        /// </summary>
+        /// <param name="gameTime">The GameTime</param>
+        public void Update(GameTime gameTime)
+        {
+            //Update keyabord state
+            keyboardState = Keyboard.GetState();
+
+            //Player 1 controls with gamepad
+            gamePadState = GamePad.GetState(PlayerIndex.One);
+            position.Y += gamePadState.ThumbSticks.Left.Y * 5;
+
+            //Player 2 contols with arrow keys
+            if (keyboardState.IsKeyDown(Keys.Up)) position.Y -= 5;
+            else if (keyboardState.IsKeyDown(Keys.Down)) position.Y += 5;
+
+            position.Y = MathHelper.Clamp(position.Y, 0, screenHeight - texture.Height);
+        }
+    }
+}
